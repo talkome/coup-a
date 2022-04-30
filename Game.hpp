96ec38f969647ab8164c1c50d02d781a2508ca03
talkome <talkome@gmail.com>
2022-04-30 23:09:50 +0300

    first

---
+++ Game.hpp
@@ -0,0 +1,36 @@
+/**
+ * Created by talko on 4/24/22.
+ */
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+#ifndef MY_SYMBOL_HH
+#define MY_SYMBOL_HH
+
+namespace coup{
+
+    class Game {
+
+        public:
+            vector<string> players_names;
+            int i;
+
+            string win;
+
+            Game(){
+                win = "playing";
+                i = 0;
+            }
+
+            vector<string> players() const;
+            string turn() const;
+            string winner() const;
+    };
+}
+
+#endif
+
